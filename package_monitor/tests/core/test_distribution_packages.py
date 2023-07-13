b4ba8af3a34b7b0efc89cb158afc57f8076e418c
ErikKalkoken <erik.kalkoken@gmail.com>
2023-07-13 19:58:31 +0200

    Make pypi update tests more focused

--- package_monitor/tests/core/test_distribution_packages.py
+++ package_monitor/tests/core/test_distribution_packages.py
@@ -10,7 +10,6 @@ from package_monitor.core.distribution_packages import (
     DistributionPackage,
     compile_package_requirements,
     gather_distribution_packages,
-    update_packages_from_pypi,
 )
 from package_monitor.tests.factories import (
     DistributionPackageFactory,
@@ -181,7 +180,6 @@ class TestUpdatePackagesFromPyPi(NoSocketsTestCase):
     def test_should_update_packages(self, requests_mocker):
         # given
         dist_alpha = DistributionPackageFactory(name="alpha", current="1.0.0")
-        packages = make_packages(dist_alpha)
         requirements = {}
         pypi_alpha = PypiFactory(distribution=dist_alpha)
         pypi_alpha.releases["1.1.0"] = [PypiReleaseFactory()]
@@ -189,19 +187,14 @@ class TestUpdatePackagesFromPyPi(NoSocketsTestCase):
             "GET", "https://pypi.org/pypi/alpha/json", json=pypi_alpha.asdict()
         )
         # when
-        update_packages_from_pypi(
-            packages=packages, requirements=requirements, use_threads=False
-        )
+        dist_alpha.update_from_pypi(requirements=requirements)
         # then
-        self.assertEqual(packages["alpha"].latest, "1.1.0")
-        self.assertEqual(
-            packages["alpha"].homepage_url, "https://pypi.org/project/alpha/"
-        )
+        self.assertEqual(dist_alpha.latest, "1.1.0")
+        self.assertEqual(dist_alpha.homepage_url, "https://pypi.org/project/alpha/")
 
     def test_should_ignore_prereleases_when_stable(self, requests_mocker):
         # given
         dist_alpha = DistributionPackageFactory(name="alpha", current="1.0.0")
-        packages = make_packages(dist_alpha)
         requirements = {}
         pypi_alpha = PypiFactory(distribution=dist_alpha)
         pypi_alpha.releases["1.1.0a1"] = [PypiReleaseFactory()]
@@ -209,16 +202,13 @@ class TestUpdatePackagesFromPyPi(NoSocketsTestCase):
             "GET", "https://pypi.org/pypi/alpha/json", json=pypi_alpha.asdict()
         )
         # when
-        update_packages_from_pypi(
-            packages=packages, requirements=requirements, use_threads=False
-        )
+        dist_alpha.update_from_pypi(requirements=requirements)
         # then
-        self.assertEqual(packages["alpha"].latest, "1.0.0")
+        self.assertEqual(dist_alpha.latest, "1.0.0")
 
     def test_should_include_prereleases_when_prerelease(self, requests_mocker):
         # given
         dist_alpha = DistributionPackageFactory(name="alpha", current="1.0.0a1")
-        packages = make_packages(dist_alpha)
         requirements = {}
         pypi_alpha = PypiFactory(distribution=dist_alpha)
         pypi_alpha.releases["1.0.0a2"] = [PypiReleaseFactory()]
@@ -226,16 +216,13 @@ class TestUpdatePackagesFromPyPi(NoSocketsTestCase):
             "GET", "https://pypi.org/pypi/alpha/json", json=pypi_alpha.asdict()
         )
         # when
-        update_packages_from_pypi(
-            packages=packages, requirements=requirements, use_threads=False
-        )
+        dist_alpha.update_from_pypi(requirements=requirements)
         # then
-        self.assertEqual(packages["alpha"].latest, "1.0.0a2")
+        self.assertEqual(dist_alpha.latest, "1.0.0a2")
 
     def test_should_not_update_package_on_network_error(self, requests_mocker):
         # given
         dist_alpha = DistributionPackageFactory(name="alpha", current="1.0.0")
-        packages = make_packages(dist_alpha)
         requirements = {}
         pypi_alpha = PypiFactory(distribution=dist_alpha)
         pypi_alpha.releases["1.1.0"] = [PypiReleaseFactory()]
@@ -246,16 +233,13 @@ class TestUpdatePackagesFromPyPi(NoSocketsTestCase):
             reason="Test error",
         )
         # when
-        update_packages_from_pypi(
-            packages=packages, requirements=requirements, use_threads=False
-        )
+        dist_alpha.update_from_pypi(requirements=requirements)
         # then
-        self.assertEqual(packages["alpha"].latest, "")
+        self.assertEqual(dist_alpha.latest, "")
 
     def test_should_ignore_yanked_releases(self, requests_mocker):
         # given
         dist_alpha = DistributionPackageFactory(name="alpha", current="1.0.0")
-        packages = make_packages(dist_alpha)
         requirements = {}
         pypi_alpha = PypiFactory(distribution=dist_alpha)
         pypi_alpha.releases["1.1.0"] = [PypiReleaseFactory(yanked=True)]
@@ -263,11 +247,9 @@ class TestUpdatePackagesFromPyPi(NoSocketsTestCase):
             "GET", "https://pypi.org/pypi/alpha/json", json=pypi_alpha.asdict()
         )
         # when
-        update_packages_from_pypi(
-            packages=packages, requirements=requirements, use_threads=False
-        )
+        dist_alpha.update_from_pypi(requirements=requirements)
         # then
-        self.assertEqual(packages["alpha"].latest, "1.0.0")
+        self.assertEqual(dist_alpha.latest, "1.0.0")
 
     @mock.patch(MODULE_PATH + ".sys")
     def test_should_ignore_releases_with_incompatible_python_requirement(
@@ -276,7 +258,6 @@ class TestUpdatePackagesFromPyPi(NoSocketsTestCase):
         # given
         mock_sys.version_info = SysVersionInfo(3, 6, 9)
         dist_alpha = DistributionPackageFactory(name="alpha", current="1.0.0")
-        packages = make_packages(dist_alpha)
         requirements = {}
         pypi_alpha = PypiFactory(distribution=dist_alpha)
         pypi_alpha.releases["1.1.0"] = [PypiReleaseFactory(requires_python=">=3.7")]
@@ -284,16 +265,13 @@ class TestUpdatePackagesFromPyPi(NoSocketsTestCase):
             "GET", "https://pypi.org/pypi/alpha/json", json=pypi_alpha.asdict()
         )
         # when
-        update_packages_from_pypi(
-            packages=packages, requirements=requirements, use_threads=False
-        )
+        dist_alpha.update_from_pypi(requirements=requirements)
         # then
-        self.assertEqual(packages["alpha"].latest, "1.0.0")
+        self.assertEqual(dist_alpha.latest, "1.0.0")
 
     def test_should_ignore_invalid_release_version(self, requests_mocker):
         # given
         dist_alpha = DistributionPackageFactory(name="alpha", current="1.0.0")
-        packages = make_packages(dist_alpha)
         requirements = {}
         pypi_alpha = PypiFactory(distribution=dist_alpha)
         pypi_alpha.releases["a3"] = [PypiReleaseFactory()]
@@ -301,11 +279,9 @@ class TestUpdatePackagesFromPyPi(NoSocketsTestCase):
             "GET", "https://pypi.org/pypi/alpha/json", json=pypi_alpha.asdict()
         )
         # when
-        update_packages_from_pypi(
-            packages=packages, requirements=requirements, use_threads=False
-        )
+        dist_alpha.update_from_pypi(requirements=requirements)
         # then
-        self.assertEqual(packages["alpha"].latest, "1.0.0")
+        self.assertEqual(dist_alpha.latest, "1.0.0")
 
 
 @Mocker()
